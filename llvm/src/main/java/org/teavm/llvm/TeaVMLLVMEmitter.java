8ae277d0862c21f66202eaafe12a2cfa6602a6a3
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-08 13:17:42 +0300

    Move LLVM-related classes to appropriate package

--- llvm/src/main/java/org/teavm/llvm/TeaVMLLVMEmitter.java
+++ llvm/src/main/java/org/teavm/llvm/TeaVMLLVMEmitter.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+package org.teavm.llvm;
 
 import java.io.IOException;
 import java.util.Arrays;
